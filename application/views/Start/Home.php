6fe96815c14cf26d4eb96b862a204a174b63e2ea
cm226 <c.matear@gmail.com>
2012-03-03 11:11:30 +0000

    small change to home page

--- application/views/Start/Home.php
+++ application/views/Start/Home.php
@@ -1,7 +1,7 @@
 <h2> Start </h2>
 <ul>
 <li><a href="../Movies/viewAll">Movies</a></li>
-<li><a href="">Music</a></li>
-<li><a href="">TV</a></li>
-<li><a href="">Pictures</a></li>
+<li><a href="../Music/viewAll">Music</a></li>
+<li><a href="../TV/viewAll">TV</a></li>
+<li><a href="../Pictures/viewAll">Pictures</a></li>
 </ul>
