f3788cfd52b477639da60e9cc41ca618d5c0ccd8
cm226 <c.matear@gmail.com>
2012-03-31 13:51:58 +0100

    added adfmin model

--- application/controllers/admincontroller.php
+++ application/controllers/admincontroller.php
@@ -9,17 +9,38 @@ class AdminController extends Controller
 	
 	function uploadTv()
 	{
-		if ($_FILES["file"]["error"] > 0)
+		if ((($_FILES["file"]["type"] == "video/avi")
+		|| ($_FILES["file"]["type"] == "video/mp4")
+		|| ($_FILES["file"]["type"] == "video/wmv"))
+		)
 		  {
-		  echo "Error: " . $_FILES["file"]["error"] . "<br />";
+		  if ($_FILES["file"]["error"] > 0)
+			{
+				$this->set("ReturnCode: ",$_FILES["file"]["error"]);
+			}
+		  else
+			{
+				$this->set("Upload", $_FILES["file"]["name"]);
+				$this->set("Type",$_FILES["file"]["type"]);
+				$this->set("Size", ($_FILES["file"]["size"] / 1024 / 1024));
+				$this->set("Temp file",$_FILES["file"]["tmp_name"]);
+
+			if (file_exists("../../Public/mix/shows/tmp/TV/" . $_FILES["file"]["name"]))
+			  {
+				$this->set("exsists",true);
+			  }
+			else
+			  {
+			  move_uploaded_file($_FILES["file"]["tmp_name"],
+			  "../public/mix/shows/tmp/TV/" . $_FILES["file"]["name"]);
+			  $this->set("Location","../../Public/mix/shows/tmp/TV/" . $_FILES["file"]["name"]);
+			  }
+			}
 		  }
 		else
 		  {
-		  echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-		  echo "Type: " . $_FILES["file"]["type"] . "<br />";
-		  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-		  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+			$this->set("InvalidFileType",true);
 		  }
-	}
+			}
 
 }
---
+++ application/views/Admin/uploadTv.php
@@ -0,0 +1,3 @@
+FileName = <?php echo $Location; ?>
+FileSize = <?php echo $Size; ?>
+FileType = <?php echo $Type;?>
