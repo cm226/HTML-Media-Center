9420d34cbc9614d9e427c9fb190e3b19c836f54c
Yan San <icy.moonlight@gmail.com>
2012-04-04 22:37:15 +0100

    Started the styling of admin

--- Desktop/application/views/Admin/Main.php
+++ Desktop/application/views/Admin/Main.php
@@ -1,5 +1,22 @@
-<ul>
-<li><a href="../Admin/Tv">Tv</a></li>
-<li><a href="../Admin/Pictures">Pictures</a></li>
-<li><a href="../Admin/Music">Music (Under Construction)</a></li>
-</ul>
+
+
+<div class="menuButton">
+
+
+<a href="../Admin/Tv">TV</a>
+
+</div>
+
+<div class="menuButton">
+
+<a href="../Admin/Pictures">Pictures</a>
+
+</div>
+
+
+<div class="menuButton">
+
+<a href="../Admin/Music">Music (Under Construction)</a>
+
+
+</div>
