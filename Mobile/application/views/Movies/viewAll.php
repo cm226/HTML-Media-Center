d0a8c3a319b2243aeb3455f5f9a14c0177690fde
unknown <icy.moonlight@gmail.com>
2012-08-20 21:49:57 +0100

    thisis yannis work on the mobile site adn she is sexy

--- Mobile/application/views/Movies/viewAll.php
+++ Mobile/application/views/Movies/viewAll.php
@@ -8,10 +8,13 @@ foreach ($movies as &$movie)
 	<div class="movieElement" onClick="window.location = '../Movies/playMovie/<?php echo $movie['Movie']['movieID'] ?>';">
 		<img class="movieImg" src="<?php echo PUBLIC_FOLDER.$movie['Movie']['thumbnailURL']; ?>"/>
 		<div class="infoPanel">
-			<h4><?php echo $movie['Movie']['movieName'];?></h4>
-			<p><?php echo $movie['Movie']['movieLength']; ?> </p>
-			<p><?php echo $movie['Movie']['movieGenre']; ?></p>
-			<p><?php echo $movie['Movie']['movieIMDB']; ?></p>
+			<table>
+			<tr><td>
+			<h4><?php echo $movie['Movie']['movieName'];?></h4></td>
+			<td><?php echo $movie['Movie']['movieLength']; ?> 
+			<?php echo $movie['Movie']['movieGenre']; ?>
+			<?php echo $movie['Movie']['movieIMDB']; ?></td></tr>
+			</table>
 		</div>
 	</div>
 	
