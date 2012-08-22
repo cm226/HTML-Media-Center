4ab22505648aa0b56edb46f933678a7d4d12dc8d
cm226 <c.matear@gmail.com>
2012-08-22 21:09:01 +0100

    added click to mobile pge

--- Mobile/application/views/Start/Home.php
+++ Mobile/application/views/Start/Home.php
@@ -11,7 +11,16 @@
 
 </div>
 <div id="metaPanel">
-
+<div class="clock">
+   <div id="Date"></div>
+      <ul>
+          <li id="hours"></li>
+          <li id="point">:</li>
+          <li id="min"></li>
+          <li id="point">:</li>
+          <li id="sec"></li>
+      </ul>
+</div>
 
 </div>
 <div id="menuSelection">
--- Mobile/application/views/Start/HomedefineIncludes.php
+++ Mobile/application/views/Start/HomedefineIncludes.php
@@ -1,7 +1,43 @@
 
 <link rel="stylesheet" type="text/css" href="../public/css/homeStyle.css">
 <script type="text/javascript" src="../public/js/searchOvelayWindows.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
 <script>
+
+$(document).ready(function() {
+// Create two variable with the names of the months and days in an array
+var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+// Create a newDate() object
+var newDate = new Date();
+// Extract the current date from Date object
+newDate.setDate(newDate.getDate());
+// Output the day, date, month and year   
+$('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+
+setInterval( function() {
+	// Create a newDate() object and extract the seconds of the current time on the visitor's
+	var seconds = new Date().getSeconds();
+	// Add a leading zero to seconds value
+	$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+	},1000);
+	
+setInterval( function() {
+	// Create a newDate() object and extract the minutes of the current time on the visitor's
+	var minutes = new Date().getMinutes();
+	// Add a leading zero to the minutes value
+	$("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
+    },1000);
+	
+setInterval( function() {
+	// Create a newDate() object and extract the hours of the current time on the visitor's
+	var hours = new Date().getHours();
+	// Add a leading zero to the hours value
+	$("#hours").html(( hours < 10 ? "0" : "" ) + hours);
+    }, 1000);	
+});
+
 
 function movieSearch(){
 	var thediv=document.getElementById('backgroundDisplay');
