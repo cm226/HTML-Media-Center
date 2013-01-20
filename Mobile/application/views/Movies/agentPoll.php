f09b8a3c5c2cae7225d9095a86821899845f6521
Craig Matear <c.matear@gmail.com>
2013-01-20 18:25:13 +0000

    merging

---
+++ Mobile/application/views/Movies/agentPoll.php
@@ -0,0 +1,13 @@
+<?php
+
+	foreach ($agents as $agent)
+	{
+		?>
+		<?php echo "'".$agent["IP"]?>
+		<div class="agent" onClick="playMovie(<?php echo "'".$agent["IP"]."', '".$movieID."'"?>);" >
+			<p>location: <?php echo $agent["Location"];?></p>
+			<p>name: <?php echo $agent["Name"];?></p>
+		</div>
+		<?php
+		
+	}
