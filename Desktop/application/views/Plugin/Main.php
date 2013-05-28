41dcd6c08010a9ab5b211c7d322f2693356ef484
Craig Matear <c.matear@gmail.com>
2013-05-28 14:11:29 +0100

    changed to upload style

--- Desktop/application/views/Plugin/Main.php
+++ Desktop/application/views/Plugin/Main.php
@@ -4,7 +4,7 @@ foreach ($plugins as $plugin)
 {
 	?>
 	<div class="plugin" onClick="window.location=' ../Plugin/pluginPage/<?php echo $plugin;?>';">
-	<h2><?php echo $plugin; ?></h2>
+	<h2 class="widgetHeader"><?php echo $plugin; ?></h2>
 	</div>
 <?php
 }
