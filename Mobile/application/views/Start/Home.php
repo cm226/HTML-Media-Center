ca61397a5d4bb2c889c1f9d99f0135ed1f9b4682
Craig Matear <c.matear@gmail.com>
2013-06-20 00:14:39 +0100

    moved start page to jquery mobile

--- Mobile/application/views/Start/Home.php
+++ Mobile/application/views/Start/Home.php
@@ -1,45 +1,11 @@
-<div id="topMetaHolder" >
-	<div class="clearfix">
-		<div id="remoteHolder" onclick="window.location = '../Shared/RemoteControl';">
-			<img src="<?php echo PUBLIC_FOLDER; ?>/img/Home/remote-Icon.png" />
-		</div>
-		<div id="dateHolder">
-		   <div id="Date"></div>
-			  <ul>
-				  <li id="hours"></li>
-				  <li id="point">:</li>
-				  <li id="min"></li>
-			  </ul>
-		</div>
-	</div>
+<div id="menuItems">
+<ul data-role="listview" data-inset="true">
+    <li><a href="../Movies/viewAll">Movies</a></li>
+    <li><a href="../Music/viewAll">Music</a></li>
+    <li><a href="../TV/viewAll">TV</a></li>
+    <li><a href="../Pictures/albums">Pictures</a></li>
+    <li><a href="../Admin/Main">Admin</a></li>
+    <li><a href="../Plugin/Main">Plugin</a></li>
+</ul>
 </div>
 
-<div id="menuSelection">
-
-<div class="tile" onclick="window.location = ('../Movies/viewAll')">
-Movie
-</div>
-
-<div class="tile" onclick="window.location = ('../Music/viewAll')">
-<p>Music</p>
-</div>
-
-<div class="tile" onClick="window.location = ('../TV/viewAll')">
-<p>TV</p>
-</div>
-
-<div class="tile" onClick="window.location = ('../Pictures/albums')">
-<p>Pictures</p>
-</div>
-
-
-<div class="tile">
-<div class="sharedTitleLeft"  onClick="window.location = ('../Admin/Main')">
-<p>Admin</p>
-</div>
-<div class="sharedTitleRight"  onClick="window.location = ('../Plugin/Main')">
-<p>Plugins</p>
-</div>
-</div>
-
-</div>
