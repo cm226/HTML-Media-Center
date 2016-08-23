348a0e9b3e0da2242ef0235298398ddcec09b74a
unknown <c.matear@gmail.com>
2016-08-23 20:44:28 +0100

    update to php V 7

--- library/sqlquery.class.php
+++ library/sqlquery.class.php
@@ -7,14 +7,10 @@ class SQLQuery {
     /** Connects to database **/
 
     function connect($address, $account, $pwd, $name) {
-        $this->_dbHandle = @mysql_connect($address, $account, $pwd);
-        if ($this->_dbHandle != 0) {
-            if (mysql_select_db($name, $this->_dbHandle)) {
+        $this->_dbHandle = mysqli_connect($address, $account, $pwd,$name);
+        if ($this->_dbHandle)
+        {
                 return 1;
-            }
-            else {
-                return 0;
-            }
         }
         else {
             return 0;
@@ -45,32 +41,31 @@ class SQLQuery {
     /** Custom SQL Query **/
 
 	function query($query, $singleResult = 0) {
-		$this->_result = mysql_query($query, $this->_dbHandle);
+		$this->_result = $this->_dbHandle->query($query);
 		
 		if (preg_match("/select/i",$query)) {
 		$result = array();
-		$table = array();
-		$field = array();
+		$tableField = array();
 		$tempResults = array();
-		$numOfFields = mysql_num_fields($this->_result);
+		$numOfFields = $this->_result->field_count;
 		for ($i = 0; $i < $numOfFields; ++$i) {
-		    array_push($table,mysql_field_table($this->_result, $i));
-		    array_push($field,mysql_field_name($this->_result, $i));
+		    array_push($tableField ,$this->_result->fetch_field_direct($i));
 		}
 
 		
-			while ($row = mysql_fetch_row($this->_result)) {
+			while ($row = $this->_result->fetch_row()) {
+				
 				for ($i = 0;$i < $numOfFields; ++$i) {
-					//$table[$i] = trim(ucfirst($table[$i]),"s");
-					$tempResults[$table[$i]][$field[$i]] = $row[$i];
+					$tempResults[$tableField[$i]->table][$tableField[$i]->name] = $row[$i];
 				}
 				if ($singleResult == 1) {
-		 			mysql_free_result($this->_result);
+		 			$this->_result->free();
 					return $tempResults;
 				}
 				array_push($result,$tempResults);
 			}
-			mysql_free_result($this->_result);
+			$this->_result->free();
+			
 			return($result);
 		}
 		
@@ -79,18 +74,18 @@ class SQLQuery {
 
     /** Get number of rows **/
     function getNumRows() {
-        return mysql_num_rows($this->_result);
+        return $this->_result->num_rows;
     }
 
     /** Free resources allocated by a query **/
 
     function freeResult() {
-        mysql_free_result($this->_result);
+        $this->_result->free();
     }
 
     /** Get error string **/
 
     function getError() {
-        return mysql_error($this->_dbHandle);
+        return $this->_dbHandle->error;
     }
 }
