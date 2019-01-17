f7eb92390ea0f9dd0c91e75e46622b643a8d7e1c
Unknown <c.matear@gmail.com>
2019-01-17 20:47:45 +0000

    updates to db config

--- db/Database.sql
+++ db/Database.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE arseholeDB;
 CREATE USER 'arsehole' IDENTIFIED BY 'pw';
-GRANT ALL privileges ON 'arseholeDB'.* TO 'arsehole'@localhost;"
+GRANT USAGE ON *.* TO 'arsehole'@'%' IDENTIFIED BY 'pw';
+FLUSH PRIVILEGES;
