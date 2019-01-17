01786ea1d2de5a970d1c2c02bdfc3010d5bf4bc1
Unknown <c.matear@gmail.com>
2019-01-17 21:32:50 +0000

    more fixes

--- db/Database.sql
+++ db/Database.sql
@@ -4,4 +4,5 @@ DROP DATABASE IF EXISTS arseholeDB;
 CREATE DATABASE arseholeDB;
 CREATE USER 'arsehole' IDENTIFIED BY 'pw';
 GRANT USAGE ON *.* TO 'arsehole'@'%' IDENTIFIED BY 'pw';
+GRANT ALL privileges ON `arseholeDB`.* TO 'arsehole'@'%';
 FLUSH PRIVILEGES;
