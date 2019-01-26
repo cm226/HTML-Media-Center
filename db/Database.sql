027f63feecd822799b53cb3723e333243f721499
Unknown <c.matear@gmail.com>
2019-01-26 18:19:41 +0000

    fixing some privalages

--- db/Database.sql
+++ db/Database.sql
@@ -3,6 +3,6 @@ DROP DATABASE IF EXISTS arseholeDB;
 
 CREATE DATABASE arseholeDB;
 CREATE USER 'arsehole' IDENTIFIED BY 'pw';
-GRANT USAGE ON *.* TO 'arsehole'@'%' IDENTIFIED BY 'pw';
-GRANT ALL privileges ON `arseholeDB`.* TO 'arsehole'@'%';
+GRANT USAGE ON *.* TO 'arsehole'@'localhost' IDENTIFIED BY 'pw';
+GRANT ALL privileges ON `arseholeDB`.* TO 'arsehole'@'localhost';
 FLUSH PRIVILEGES;
