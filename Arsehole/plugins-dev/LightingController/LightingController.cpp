3c4dd6110fb08c5dc8bdfe27d499248eb56c622e
Craig <c.matear@gmail.com>
2020-04-18 20:49:56 +0100

    only turn on light when its dark

--- Arsehole/plugins-dev/LightingController/LightingController.cpp
+++ Arsehole/plugins-dev/LightingController/LightingController.cpp
@@ -123,7 +123,6 @@ void LightingController::bedroomMotion(){
     if(s.IsSunDown()){
         turnOnLight("bedroom");
         
-
         std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
         time_t tt = std::chrono::system_clock::to_time_t(now);
         tm local_tm = *localtime(&tt);
@@ -276,7 +275,10 @@ void LightingController::setupSchedule(
         coreMod->getScheduler()
     );
     m_schedual->Initialse(17, 0, [&](){
+        SunsetTimes s;
+        if(s.IsSunDown()){
             turnOnLight("bedroom");
+        }
     });
 }
 
